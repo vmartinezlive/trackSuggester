99f8da887a64dbba20c313db717210d56ca658a8
Victoria Martinez <vmartinez72@live.com>
2018-10-19 11:39:21 -0700

    work possible js solution (code broken)

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,36 @@
-var totalYes = function(number1, number2) {
-  return number1 + number2;
+var totalYes = function(number1, number1) {
+  return number1 + number1;
+}
+var totalNo =function(number2, number2) {
+  return number2 + number2;
+debugger;
 }
 var yesProgrammer = function(totalYes) {
-  if(answer >=3) {
+  if(answer >=2) {
     return true;
   }else{
     return false;
   }
 }
+var NoProgrammer = function(totalNo) {
+    if(answer >=2) {
+      return true;
+    }else{
+      return false;
+    }
+}
 
-S(document).ready(function() {
+$(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#input1").val());
 		var number2 = parseInt($("#input2").val());
     if (totalYes === "yesProgrammer") {
-      $(".programmer").show();
-      $(".no-go").hide();
+      $("#programmer").show();
+      $("#no-go").hide();
     } else {
-      $(".no-go").show();
-      $(".programmer").hide();
+      $("#no-go").show();
+      $("#programmer").hide();
     }
   });
 });
