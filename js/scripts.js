dea75b0e8b7ccba3bda1f5a0b156a0e0a449705f
Victoria Martinez <vmartinez72@live.com>
2018-10-19 10:18:41 -0700

    change forms to selct boxes style forms

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var totalYes = function(yes1, yes2, yes3, yes4, yes5) {
+  return yes1 + yes2 + yes3 + yes4 + yes5;
+  };
+
+
+
+
+
+
+
+
+
+
+S(document).ready(function() {
+  $().submit(function(event) {
+
+  });
+});
